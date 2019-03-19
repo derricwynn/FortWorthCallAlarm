9b7e2f1381fb6691afab5f63fb44450dc845e8fe
derricwynn <48457310+derricwynn@users.noreply.github.com>
2019-03-18 22:29:15 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Fort_Worth_Call_Alarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 159);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ENGAGE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ALARM IS SET";
+            this.label1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "RESET";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Running...";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 369);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Derric\'s FW Call Alarm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+using System.Diagnostics;
+
+
+namespace Fort_Worth_Call_Alarm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // This program once the user clicks engage will check to see if notepad is running
+        // if notepad is running then nothing should happen and it should keep checking
+        // if notepad is not running it should play an alarm sound
+
+            //bool wait acts as a flag for method
+        static bool wait;
+
+        //appRunning method is what checks for specified process running
+        public void appRunning(string appName = "communicatork9")          // Replace appName = "_____" <-- enter what ever process listed in taskmanager without the .exe
+        {
+
+            
+         
+            Process[] ProcessList = Process.GetProcesses();
+
+            foreach (Process p in ProcessList)
+            {
+
+                   
+                if (p.ProcessName.Contains(appName))
+                {
+
+                    wait = true;
+                    break;
+
+                    
+
+                }
+                else
+                {
+                    
+                
+                    wait = false;
+                    
+                }
+                if (wait)
+                {
+
+                    checkAgain(appName);
+                }
+                
+
+            }
+            
+        
+            
+        }
+        public void checkAgain(string p)
+        {
+          
+            appRunning(p);
+        }
+
+        public void ringthealarm(bool wait = false)
+        {
+            if (wait == false)
+            {
+
+                   SoundPlayer alarm = new SoundPlayer(@"C:\windows\media\alarm10.wav");
+                   alarm.PlayLooping();
+               
+            }
+            else
+            {
+               if(wait)
+                {
+
+
+                }
+                    
+                    }
+
+        }
+
+
+        // Engage button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+               label1.Visible = true;
+               label1.ForeColor = Color.Red;
+            
+            appRunning();
+               label2.Visible = true;
+            MessageBox.Show("monitoring");       // For some reason program crashes when
+            while (wait)
+            {
+
+                appRunning();
+
+            }
+            if (wait == false)
+            {
+
+                ringthealarm();
+
+            }
+
+
+
+        }
+
+        
+                    
+                    
+           
+                       
+        
+                
+         // RESET BUTTON
+        private void button2_Click(object sender, EventArgs e)
+        {
+         
+            label1.Visible = false;
+            SoundPlayer stopsound = new SoundPlayer(@"C\windows\media\Alarm10.wav");
+            stopsound.Stop();
+            label2.Visible = false;
+            
+        }
+
+    }
+}
+
